d3f7cfc01724bfbacc472816be281269acd18e04
roxmulder <we.are.torchwood@gmail.com>
2017-05-01 15:41:04 -0300

    Agrego funciones de memoria en el Utils.

---
+++ UtilsLibrary/src/utils_memoria.c
@@ -0,0 +1,182 @@
+/*
+ * utils_memoria.c
+ *
+ *  Created on: 1/5/2017
+ *      Author: utnso
+ */
+
+#include "utils_memoria.h"
+
+t_memoria * new_memoria(int marcos, int marcosSize){
+
+	t_memoria * memoria = malloc(sizeof(t_memoria));
+	if (memoria == NULL) {
+		return NULL;
+	}
+
+	memoria->memoria_principal = (void *) calloc (marcos, marcosSize); //mmmm
+	if (memoria->memoria_principal == NULL) {
+		free(memoria);
+		return NULL;
+	}
+
+	memoria->paginas = malloc(sizeof(t_pagina) * marcos);
+	if (memoria->paginas == NULL) {
+		free(memoria->memoria_principal);
+		free(memoria);
+		return NULL;
+	}
+	int i;
+	for(i = 0; i < marcos; i++){
+		memoria->paginas[i].nro_marco = i;
+		memoria->paginas[i].pid = -1; //no está asignada a ningún proceso
+		memoria->paginas[i].nro_pagina = i; //le asignamos un nro como para poner algo
+	}
+
+	memoria->programas = list_create();
+	if(memoria->programas == NULL){
+		free(memoria->paginas);
+		free(memoria->memoria_principal);
+		free(memoria);
+		return NULL;
+	}
+
+	memoria->cantidad_marcos = marcos;
+	memoria->tamanio_marco = marcosSize;
+	memoria->cantidad_paginas_disponibles = marcos;
+
+
+	return memoria;
+}
+
+//PREGUNTAR EN LA CACHÉ
+void * solicitar_bytes_de_una_pagina(t_memoria * memoria, int nro_pagina, int offset, int tamanio){
+	void * bytes = malloc(tamanio);
+	memcpy(bytes, devolver_pagina(memoria, nro_pagina) + offset, tamanio);
+	return bytes;
+}
+
+//ACTUALIZAR EN CACHÉ TMB
+void almacenar_bytes_en_una_pagina(t_memoria * memoria, int nro_pagina, int offset, int tamanio, void * buffer){
+	memcpy(devolver_pagina(memoria, nro_pagina) + offset, buffer, tamanio);
+}
+
+//Dado un idPagina, busca la pagina en la memoria y la devuelve
+void * devolver_pagina(t_memoria * memoria, int nro_pagina){
+	int i = 0;
+	while(memoria->paginas[i].nro_pagina != nro_pagina){
+		i++;
+	}
+	int nro_marco = memoria->paginas[i].nro_marco;
+	int tamanio_marco = memoria->tamanio_marco;
+	void * pagina = memoria->memoria_principal + nro_marco * tamanio_marco; //chequear
+	return pagina;
+}
+
+//arreglar :///
+//void * asignar_paginas_a_proceso(t_memoria * memoria, pid_t pid, int paginas_requeridas){
+//	if(paginas_requeridas <= memoria->cantidad_paginas_disponibles){
+//		int i = 0;
+//		int j = 0; //buscar el ultimo nro de vector del pid
+//		int * paginas_asignadas = malloc(sizeof(int) * paginas_requeridas);
+//		if (paginas_asignadas == NULL) {
+//			printf("paginas asignadas null");
+//			return NULL;
+//		}
+//		while (paginas_requeridas > 0) {
+//			if (memoria->paginas[i].pid > -1) {
+//				asignar_pagina(memoria, pid, i, j);
+//				paginas_requeridas--;
+//				paginas_asignadas[j] = memoria->paginas[i].nro_marco;
+//				j++;
+//			}
+//			i++;
+//		}
+//	} else{
+//		return NULL;
+//	}
+//}
+void * inicializar_programa(t_memoria * memoria, pid_t pid, int paginas_requeridas){ //, void * codigoPrograma
+	if(paginas_requeridas <= memoria->cantidad_paginas_disponibles){
+
+		int i = 0;
+		int j = 0;
+		int * paginas_asignadas = malloc(sizeof(int) * paginas_requeridas);
+		if(paginas_asignadas == NULL){
+			printf("paginas asignadas null");
+			return NULL;
+		}
+		while(paginas_requeridas > 0){
+			if(memoria->paginas[i].pid > -1){
+				asignar_pagina(memoria, pid, i, j);
+				paginas_requeridas --;
+				paginas_asignadas[j] = memoria->paginas[i].nro_marco;
+				j++;
+			}
+			i++;
+		}
+		t_entrada_programa * nuevo_programa = malloc(sizeof(t_entrada_programa));
+		if(nuevo_programa == NULL){
+			printf("nuevo programa null");
+			free(paginas_asignadas);
+			return NULL;
+		}
+		nuevo_programa->pid = pid;
+		nuevo_programa->cantidad_paginas = paginas_requeridas;
+		nuevo_programa->paginas_asignadas = paginas_asignadas;
+		list_add(memoria->programas, nuevo_programa);
+		return nuevo_programa;
+	}
+	else{
+		return NULL;
+	}
+}
+
+void asignar_pagina(t_memoria * memoria, pid_t pid, int posicion_tabla, int nro_pagina){//nro_pagina es dentro del programa
+	memoria->paginas[posicion_tabla].pid = pid;
+	memoria->paginas[posicion_tabla].nro_pagina = nro_pagina;
+	memoria->cantidad_paginas_disponibles--;
+}
+
+void liberar_pagina(t_memoria * memoria, int posicion_tabla){
+	memoria->paginas[posicion_tabla].pid = -1;
+	memoria->cantidad_paginas_disponibles++;
+}
+
+void finalizar_programa(t_memoria * memoria, pid_t pid){
+	int i;
+	for(i = 0; i < memoria->cantidad_marcos; i++){
+		if(memoria->paginas[i].pid == pid){
+			liberar_pagina(memoria, i);
+		}
+	}
+	//eliminar de la lista de programas la entrada q corresponde al pid
+}
+
+t_entrada_cache * new_cache(int entradas_cache, int marco_size){
+	t_entrada_cache * cache = malloc((sizeof(pid_t) + sizeof(int) + marco_size) * entradas_cache);
+	if(cache == NULL){
+		return NULL;
+	}
+
+	int i;
+	for(i = 0; i < entradas_cache; i++){
+		//cache[i]->pid = -1;
+	}
+
+	return cache;
+}
+
+//falta terminar
+void reemplazo_pagina(t_pagina pagina, t_entrada_cache * cache, t_memoria * memoria){
+//	int entrada = 0;
+//	while(cache[entrada]->pid != -1){
+//		entrada++;
+//	}
+//	if(cache[entrada]->pid == -1){
+//		cache[entrada]->pid = pagina->pid;
+//		cache[entrada]->nro_pagina = pagina->nro_pagina;
+//		cache[entrada]->contenido_pagina = devolver_pagina(memoria, pagina->nro_pagina);
+//	}
+}
+
---
+++ UtilsLibrary/src/utils_memoria.h
@@ -0,0 +1,64 @@
+/*
+ * utils_memoria.h
+ *
+ *  Created on: 1/5/2017
+ *      Author: utnso
+ */
+
+#ifndef SRC_UTILS_MEMORIA_H_
+#define SRC_UTILS_MEMORIA_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <commons/collections/list.h>
+#include <sys/types.h>
+
+typedef struct{
+	int nro_marco;
+	pid_t pid; //proceso al que pertenece la pagina
+	int nro_pagina; //orden de la pagina del proceso
+} t_pagina;
+
+typedef struct{
+	pid_t pid;
+	int * paginas_asignadas;
+	int cantidad_paginas;
+} t_entrada_programa;
+
+typedef struct{
+	void * memoria_principal;
+	t_pagina * paginas;
+	int tamanio_marco;
+	int cantidad_marcos; //es igual a la cantidad de paginas (1 pagina 1 marco)
+	int cantidad_paginas_disponibles;
+	t_list * programas;
+} t_memoria;
+
+typedef struct{
+	pid_t pid;
+	int nro_pagina;
+	void * contenido_pagina; //el tipo está bien?
+} t_entrada_cache;
+
+t_memoria * new_memoria(int marcos, int marcosSize);
+
+void * solicitar_bytes_de_una_pagina(t_memoria * memoria, int nro_pagina, int offset, int tamanio);
+
+void almacenar_bytes_en_una_pagina(t_memoria * memoria, int nro_pagina, int offset, int tamanio, void * buffer);
+
+void * devolver_pagina(t_memoria * memoria, int nro_pagina);
+
+void * inicializar_programa(t_memoria * memoria, pid_t pid, int paginas_requeridas);
+
+void asignar_pagina(t_memoria * memoria, pid_t pid, int posicion_tabla, int nro_pagina);
+
+void liberar_pagina(t_memoria * memoria, int posicion_tabla);
+
+void finalizar_programa(t_memoria * memoria, pid_t pid);
+
+t_entrada_cache * new_cache(int entradas_cache, int marco_size);
+
+void reemplazo_pagina(t_pagina pagina, t_entrada_cache * cache, t_memoria * memoria);
+
+#endif /* SRC_UTILS_MEMORIA_H_ */
