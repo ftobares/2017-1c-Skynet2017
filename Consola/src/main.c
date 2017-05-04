e4f57b7fc881e0a232c48fd144e72c825a205d18
JadirG <jadir.gomez@gmail.com>
2017-05-03 22:06:38 -0300

    Handshake parcial de la consol al CPU, falta corregir cuando no se hace el handshake correcto

--- Consola/src/main.c
+++ Consola/src/main.c
@@ -12,10 +12,15 @@
 
 #define PACKAGESIZE 1024
 #define TIPO_PROYECTO 1
+#define BUFFERSIZE 40000
+#define HSKERNEL     2 //HANDSHAKE
+#define TOCPUKERNEL  3 //TIPO
+#define OK 	"1"
 
 //Variables Globales
 t_console_config* config;
 t_socket server_socket;
+int CONECTADOALKERNEL = 0;
 
 //Declaracion de funciones
 void conectar_al_kernel();
@@ -32,24 +37,95 @@ int main(int argc, char* argv) {
 	return 0;
 }
 
+int Enviar(int sRemoto, char * buffer)
+{
+  int cantBytes;
+  cantBytes = send(sRemoto, buffer, strlen(buffer), 0);
+  if (cantBytes == -1)
+    perror("ERROR ENVIO DATOS.\n");
+  return cantBytes;
+}
+
+int Recibir(int sRemoto, char * buffer)
+{
+  int bytecount;
+  memset(buffer, 0, BUFFERSIZE);
+  if ((bytecount = recv(sRemoto, buffer, BUFFERSIZE, 0)) == -1)
+    perror("ERROR RECIBO DATOS. \n");
+
+  return bytecount;
+}
+
+int saludar(int handshake, int tipo, int sRemoto) {
+
+	char *respuesta = malloc(BUFFERSIZE * sizeof(char));
+	char *mensaje = string_new();
+	string_append(&mensaje, "X");
+	string_append(&mensaje, string_itoa(handshake));
+	string_append(&mensaje, string_itoa(tipo));
+	int aux;
+
+	Enviar(sRemoto, mensaje);
+	Recibir(sRemoto, respuesta);
+
+	if (!(string_starts_with(respuesta, OK)))
+	{
+		perror("ERROR: HANDSHAKE NO FUE EXITOSO \n");
+	}
+	else
+		aux = 0;
+
+	if (mensaje != NULL)
+		free(mensaje);
+	if (respuesta != NULL)
+		free(respuesta);
+
+	return aux;
+}
+
+
 void conectar_al_kernel() {
 
-	server_socket = cliente_crear_socket(config->ip_kernel, config->puerto_kernel);
+	server_socket = cliente_crear_socket(config->ip_kernel,
+			config->puerto_kernel);
 
-	connect(server_socket.socket, server_socket.socket_info->ai_addr, server_socket.socket_info->ai_addrlen);
+	int AUX_CONEC_KER = connect(server_socket.socket,
+			server_socket.socket_info->ai_addr,
+			server_socket.socket_info->ai_addrlen);
 
-	freeaddrinfo(server_socket.socket_info); // No lo necesitamos mas
-
-	int enviar = 1;
-	char message[PACKAGESIZE];
-	printf("Conectado al servidor. Ya puede enviar mensajes. Escriba 'exit' para salir\n");
-	while (enviar) {
-		fgets(message, PACKAGESIZE, stdin); // Lee una linea en el stdin (lo que escribimos en la consola) hasta encontrar un \n (y lo incluye) o llegar a PACKAGESIZE.
-		if (!strcmp(message, "exit\n"))
-			enviar = 0; // Chequeo que el usuario no quiera salir
-		if (enviar)
-			send(server_socket.socket, message, strlen(message) + 1, 0); // Solo envio si el usuario no quiere salir.
+	if (AUX_CONEC_KER < 0) {
+		printf("Error en connect CONSOLA -> KERNEL\n");
+		exit(-1);
 	}
 
-	close(server_socket.socket);
+	//HandShake CONSOLA -> KERNEL
+	if (AUX_CONEC_KER == saludar(HSKERNEL, TOCPUKERNEL, server_socket.socket)) {
+		CONECTADOALKERNEL = 1;
+	}
+
+	connect(server_socket.socket, server_socket.socket_info->ai_addr,
+			server_socket.socket_info->ai_addrlen);
+
+	freeaddrinfo(server_socket.socket_info); // No lo necesitamos mas
+	printf("CONECTADO AL KERNEL: %d", CONECTADOALKERNEL);
+
+	if (CONECTADOALKERNEL) {
+		int enviar = 1;
+		char message[PACKAGESIZE];
+		printf(
+				"Conectado al servidor. Ya puede enviar mensajes. Escriba 'exit' para salir\n");
+		while (enviar) {
+			fgets(message, PACKAGESIZE, stdin); // Lee una linea en el stdin (lo que escribimos en la consola) hasta encontrar un \n (y lo incluye) o llegar a PACKAGESIZE.
+			if (!strcmp(message, "exit\n"))
+				enviar = 0; // Chequeo que el usuario no quiera salir
+			if (enviar)
+				send(server_socket.socket, message, strlen(message) + 1, 0); // Solo envio si el usuario no quiere salir.
+		}
+
+	} else {
+
+		close(server_socket.socket);
+		perror("HANDSHAKE ERROR - No se pudo conectar al KERNEL");
+		return;
+	}
 }
--- Kernel/src/main.c
+++ Kernel/src/main.c
@@ -12,16 +12,19 @@
 #include <arpa/inet.h> //inet_addr
 #include <src/utils_config.h>
 #include <src/utils_socket.h>
+
 #define BACKLOG 30	// Cantidad conexiones maximas
 #define PACKAGESIZE 1024	// Size maximo del paquete a enviar
 #define TRUE 1
 #define FALSE 0
 #define CantClientes 30
 #define TIPO_PROYECTO 4
-#define MSJ_HANDSHAKE 'H'
+#define MSJ_HANDSHAKE_CPU 'H'
+#define MSJ_HANDSHAKE_CONSOLA 'C'
 #define MSJ_CONFIRMACION "1"
 #define MSJ_NEGACION     "0"
 #define HANDSHAKE_CPU '1'
+#define HANDSHAKE_CONSOLA '2'
 //Variables Globales
 t_kernel_config* config;
 t_socket socket_memoria;
@@ -29,6 +32,45 @@ t_socket socket_fs;
 t_master_socket master_socket;
 //Declaracion de funciones
 int iniciar_servidor();
+
+typedef struct
+{
+	int offsetInicio;
+	int offsetFin;
+} indiceCodigo;
+
+typedef struct
+{
+ int etiqueta; //Verificar
+} indiceEtiquetas;
+
+typedef struct
+{
+	int nroPagina;
+	int offset;
+	int size;
+}	posicionDeMemoria;
+
+typedef struct
+{
+	t_list* listaDeArgumentos;
+	t_list* listaDeVariables;
+	int retPos;
+	posicionDeMemoria retVar;
+
+} indiceStack;
+
+typedef struct
+{
+	pid_t pid;
+	int programCounter;
+	int paginasCodigo;
+	indiceCodigo indiceCodigo;
+	indiceEtiquetas etiquetas;
+	indiceStack stack;
+	int exitCode;
+} PCB;
+
 
 int main(int argc, char** argv) {
 
@@ -48,6 +90,19 @@ int EnviarDatos(int socket, void *buffer) {
 
 	return bytecount;
 }
+
+void inicializarPCB(PCB* auxPCB) {
+	auxPCB->pid = 0;
+	auxPCB->programCounter = 0;
+	auxPCB->paginasCodigo = 0;
+	auxPCB->indiceCodigo.offsetFin = 0;
+	auxPCB->indiceCodigo.offsetInicio = 0;
+	auxPCB->etiquetas.etiqueta = 0;
+	auxPCB->stack.listaDeArgumentos = list_create();
+	auxPCB->stack.listaDeVariables = list_create();
+	auxPCB->exitCode = 0;
+}
+
 
 int iniciar_servidor() {
 	int opt = TRUE;
@@ -143,14 +198,23 @@ int iniciar_servidor() {
 					int j;
 					mensaje[valorLectura] = '\0';
 					tipo_mensaje = mensaje[0];
-					printf("MENSAJE CPU: %s\n", mensaje);
+					printf("MENSAJE: %s\n", mensaje);
 					switch (tipo_mensaje) {
-					case MSJ_HANDSHAKE:
+					case MSJ_HANDSHAKE_CPU:
 						if(mensaje[1] == HANDSHAKE_CPU)
 							EnviarDatos(sd, MSJ_CONFIRMACION);
 						else
 							EnviarDatos(sd, MSJ_NEGACION);
 						break;
+					case MSJ_HANDSHAKE_CONSOLA:
+						if(mensaje[1] == HANDSHAKE_CONSOLA)
+						{
+							puts("ENTRE CON LA CONSOLA");
+							EnviarDatos(sd, MSJ_CONFIRMACION);
+						}
+						else
+							EnviarDatos(sd, MSJ_NEGACION);
+					break;
 
 					default:
 						/* send to everyone! */
